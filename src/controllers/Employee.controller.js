7350b2088e9ecaf364fa4160223bdb7e24084e47
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-05-21 16:42:20 +0530

    some change

--- src/controllers/Employee.controller.js
+++ src/controllers/Employee.controller.js
@@ -2,21 +2,39 @@ import { StatusCodes } from "http-status-codes";
 import { FileUrl } from "../constant.js";
 import { EmployeeModel } from "../models/Employee.model.js";
 import { AsyncHandler } from "../utils/AsyncHandler.js";
+import { NotFoundError } from "../utils/CustomError.js";
 
 
 
-export const CreateEmployeeDetail = AsyncHandler(async (req,res) => {
-    const {body,files} = req;
+export const CreateEmployeeDetail = AsyncHandler(async (req, res) => {
+    const { body, files } = req;
 
-    const aadhaar = `${FileUrl}/${files.aadhaar[0].filename}`
-    const photo = `${FileUrl}/${files.photo[0].filename}`
-    const pancard = `${FileUrl}/${files.pancard[0].filename}`
+    if (!files?.aadhaar) {
+        throw new NotFoundError("Aadhaar Card image not found", "CreateEmployeeDetail method");
+    }
 
-    const result = await EmployeeModel.create({...body,aadhaar,photo,pancard});
+    if (!files.photo) {
+        throw new NotFoundError("Photo not found", "CreateEmployeeDetail method");
+    }
+
+    if (!files.pancard) {
+        throw new NotFoundError("Pancard Proof not found", "CreateEmployeeDetail method");
+    }
+
+    if (!files.Bank_Proof) {
+        throw new NotFoundError("Bank Proof not found", "CreateEmployeeDetail method");
+    }
+
+    const aadhaar = `${FileUrl}/${files.aadhaar[0].filename}`;
+    const photo = `${FileUrl}/${files.photo[0].filename}`;
+    const pancard = `${FileUrl}/${files.pancard[0].filename}`;
+    const Bank_Proof = `${FileUrl}/${files.Bank_Proof[0].filename}`;
+
+    const result = await EmployeeModel.create({ ...body, aadhaar, photo, pancard,Bank_Proof });
 
     return res.status(StatusCodes.CREATED).json({
-        message:"Employee data Uploaded",
-        data:result
+        message: "Employee data Uploaded",
+        data: result
     })
 
 });
--- src/routes/Employee.routes.js
+++ src/routes/Employee.routes.js
@@ -1,14 +1,20 @@
 import { Router } from "express";
 import { CreateEmployeeDetail } from "../controllers/Employee.controller.js";
 import { upload } from "../config/multer.config.js";
+import { Autherization } from "../middleware/Autherization.js";
+import { Validater } from "../helper/Validator.js";
+import { EmployeeValidation } from "../validation/Employee.validation.js";
 
 const routes = Router();
 
-routes.route("/add-data").post(upload.fields([
-    {name:"aadhaar",maxCount:1},
-    {name:"photo",maxCount:1},
-    {name:"pancard",maxCount:1},
-]),CreateEmployeeDetail)
+routes.route("/add-data").post(Autherization, upload.fields([
+    { name: "aadhaar", maxCount: 1 },
+    { name: "photo", maxCount: 1 },
+    { name: "pancard", maxCount: 1 },
+    { name: "Bank_Proof", maxCount: 1 },
+    { name: "Voter_Id", maxCount: 1 },
+    { name: "Driving_Licance", maxCount: 1 },
+]), Validater(EmployeeValidation), CreateEmployeeDetail)
 
 
 export default routes;
