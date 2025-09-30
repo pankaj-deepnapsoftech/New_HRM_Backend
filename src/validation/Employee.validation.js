16e36d17955dd2bce979cef9dcb9e769bdeca6bd
nitinthakurdev <radheradhe9870023@gmail.com>
2025-09-30 10:38:25 +0530

    new changes

--- src/validation/Employee.validation.js
+++ src/validation/Employee.validation.js
@@ -3,14 +3,14 @@ import {string,object,number} from "yup";
 
 
 export const EmployeeValidation = object({
-    Address:string().required("Address is required field"),
-    Department:string().required("Department is required field"),
-    Designation:string().required("Designation is required field"),
+    // Address:string().required("Address is required field"),
+    // Department:string().required("Department is required field"),
+    // Designation:string().required("Designation is required field"),
     UAN_number:string().required("UAN Number is required field"),
     Bank_Account:string().required("Bank Account is required field"),
     Back_Name:string().required("Back Name is required field"),
     IFSC_Code:string().required("IFSC Codeis required field"),
-    salary:number().required("Salary is required field"),
+    // salary:number().required("Salary is required field"),
 });
 
 
