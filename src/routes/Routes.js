fc3a0ed8ba835713801dbd58a209eb135a53d298
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-10-01 12:53:16 +0530

    sdf

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,33 +5,29 @@
 // import EmployeeRoutes from "./Employee.routes.js";
 // import ProjectRoutes from "./ProjectRoutes.js";
 
-
 // const router = Router();
 
 // router.use("/user",UserRoutes)
 // router.use("/employee",EmployeeRoutes)
 // router.use("/projects", ProjectRoutes);
 
+// export default router;
 
-// export default router; 
-
-import { Router } from "express";
-import UserRoutes from "./Auth.routes.js";
-import EmployeeRoutes from "./Employee.routes.js";
-import ProjectRoutes from "./ProjectRoutes.js";
-import UsersRoutes from "./UserRoutes.js"; 
-import EmpDataRoutes from "./EmpDataRoutes.js"; 
-import DepartmentRoutes from "./DepartmentsRoutes.js";
-import LeaveRoutes from './leaveRoutes.js'
+import { Router } from 'express';
+import UserRoutes from './Auth.routes.js';
+import EmployeeRoutes from './Employee.routes.js';
+import ProjectRoutes from './ProjectRoutes.js';
+import UsersRoutes from './UserRoutes.js';
+import EmpDataRoutes from './EmpDataRoutes.js';
+import DepartmentRoutes from './DepartmentsRoutes.js';
+import LeaveRoutes from './leaveRoutes.js';
 
 const router = Router();
-router.use("/user", UserRoutes);
-router.use("/employee", EmployeeRoutes);
-router.use("/projects", ProjectRoutes);
-router.use("/users", UsersRoutes); 
-router.use("/empdata", EmpDataRoutes); 
-router.use("/departments",DepartmentRoutes);
-router.use("/leaves",LeaveRoutes)
+router.use('/user', UserRoutes);
+router.use('/employee', EmployeeRoutes);
+router.use('/projects', ProjectRoutes);
+router.use('/users', UsersRoutes);
+router.use('/empdata', EmpDataRoutes);
+router.use('/departments', DepartmentRoutes);
+router.use('/leaves', LeaveRoutes);
 export default router;
-
-
